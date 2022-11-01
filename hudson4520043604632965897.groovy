9f00666177020880ebb57c574145208868edaace
udtrucks <116274863+udtrucks@users.noreply.github.com>
2022-11-01 14:12:00 +0100

    Add files via upload

---
+++ hudson4520043604632965897.groovy
@@ -0,0 +1,15 @@
+nexusArtifactUploader(
+    nexusVersion: 'nexus3',
+    protocol: 'http',
+    nexusUrl: 'http://10.180.97.196:8081/',
+    groupId: 'com.example',
+    version: '1.0.1.00',
+    repository: 'vvk_new',
+    credentialsId: 'CredentialsId',
+    artifacts: [
+        [artifactId: Newproject,
+         classifier: '',
+         file: 'my-service-' + version + '.jar',
+         type: 'jar']
+    ]
+ )
